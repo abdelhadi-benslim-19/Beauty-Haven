230b77867d5d4716dabcc4daf79b625af78eeb5c
abdelhadi-benslim-19 <82743999+abdelhadi-benslim-19@users.noreply.github.com>
2024-07-10 14:40:50 +0100

    updated

--- script.js
+++ script.js
@@ -11,3 +11,18 @@ fetch('footer.html')
     .then(data => {
         document.getElementById('footer-container').innerHTML = data;
     });
+
+// swapping effect for login and register forms
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('show-register').addEventListener('click', function(e) {
+        e.preventDefault();
+        document.getElementById('login-form').classList.add('d-none');
+        document.getElementById('register-form').classList.remove('d-none');
+    });
+
+    document.getElementById('show-login').addEventListener('click', function(e) {
+        e.preventDefault();
+        document.getElementById('login-form').classList.remove('d-none');
+        document.getElementById('register-form').classList.add('d-none');
+    });
+});
